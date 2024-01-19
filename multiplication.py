bc5b72c5c293bc0ab56b086400b9ef39756163bb
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2024-01-19 15:21:28 -0800

    Create multiplication.py

---
+++ multiplication.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def main():
+    # Enter your solution here
+    for n in range(11):
+        result = 4*n
+        print("4 multiplied by", n ,"is ", result )
+
+if __name__ == "__main__":
+    main()
