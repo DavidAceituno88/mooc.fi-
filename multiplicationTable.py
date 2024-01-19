8280719d3254660b1c78f873d073c5cbc80b6d5b
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2024-01-19 15:21:03 -0800

    Create multiplicationTable.py

---
+++ multiplicationTable.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+def main():
+   
+   for x in range (1,11):
+    for y in range (1,11):
+      print(x*y, end="\t")
+    print()
+    pass
+
+if __name__ == "__main__":
+    main()
