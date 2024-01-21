b5ef5a8cfca256008635d5b1e6b165b095648b23
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2024-01-21 15:43:20 -0800

    Create twoDice.py

---
+++ twoDice.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+    for x in range (1,7):
+        for y in range(1,7):
+            if x + y == 5:
+                print(x,",",y)
+    pass
+
+if __name__ == "__main__":
+    main()
