c936c00c6dc48213095e5a7a47d2115fb8e06c5f
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2024-01-28 07:25:27 -0800

    Update multiplication.py

--- multiplication.py
+++ multiplication.py
@@ -3,8 +3,9 @@
 def main():
     # Enter your solution here
     for n in range(11):
-        result = 4*n
-        print("4 multiplied by", n ,"is ", result )
+        r = 4*n
+        print(f"4 multiplied by {n} is {r}" )
 
 if __name__ == "__main__":
     main()
+
