7563232558eeade044cc30d0af1d2c5c2cf02723
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2024-01-27 16:58:57 -0800

    Update areas_of_shapes.py

--- areas_of_shapes.py
+++ areas_of_shapes.py
@@ -4,7 +4,7 @@ import math
 
 
 def main():
-    n = False
+    n = True
     while n == True:
         shape = input("Choose a shape (triangle,rectangle,circle): ")
         if shape == "triangle":
@@ -12,14 +12,23 @@ def main():
             base= float(input("Give base of triangle:"))
             height = float(input("Give height of triangle:"))
             area = (height*base)/2
-            print(area)
+            print("The area is ",area)
         elif shape == "circle":
             n = True
             radius = float(input("Give radius of circle:"))
-            area = (math.pi*radius)**2
-            print(area)
+            area = math.pi*(radius**2)
+            print("The area is ",area)
+        elif shape == "rectangle":
+            n = True
+            height = float(input("Give height of the rectangle:"))
+            width = float(input("Give width of the rectangle:"))
+            area = (height*width)
+            print("The area is ",area)
         elif shape == '':
             n=False
+        else:
+            n=True
+            print("Unknown shape!")
 
     # enter you solution here
 
